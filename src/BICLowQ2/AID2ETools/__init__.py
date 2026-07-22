58f28fcafbc2ecdba239c5c5f363f3537f005eaf
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-07-22 15:24:10 +0100

    Integrate option parsing

--- src/BICLowQ2/AID2ETools/__init__.py
+++ src/BICLowQ2/AID2ETools/__init__.py
@@ -8,10 +8,19 @@ simplify interfacing with Ax, etc.
 __version__="0.0.0"
 
 from .AxHelper import *
+from .OptionParser import *
 
 __all__ = [
     "ConvertParamConfig",
     "ConverParamList",
     "ConvertObjectConfig",
-    "CreateObjectiveNames"
+    "CreateObjectiveNames",
+    "GetConfigPath",
+    "GetConfigPaths",
+    "GetMoboPath",
+    "GetThisMobo",
+    "GetSlurmTemplate",
+    "LoadConfig",
+    "LoadConfigs",
+    "ParseArguments",
 ]
